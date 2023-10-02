18598cea68d4b48d427961623bf237829612b70b
Marcin <feleck@users.noreply.github.com>
2023-10-02 14:10:14 +0200

    33 Upload index.html only if a content_version has changed (#34)

--- modules/terrahouse-aws/variables.tf
+++ modules/terrahouse-aws/variables.tf
@@ -37,3 +37,12 @@ variable "error_html_filepath" {
     error_message = "The provided path for erroe.html does not exist."
   }
 }
+
+variable "content_version" {
+  description = "The content version"
+  type = number
+  validation {
+    condition = var.content_version> 0 && floor(var.content_version) == var.content_version
+    error_message = "The content_version must be a positive integer starting at 1"
+  }
+}
