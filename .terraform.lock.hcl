5e444bdd4164f59cdb175d7dfb1c94bba265e3cb
Marcin <feleck@users.noreply.github.com>
2023-10-11 12:24:45 +0200

    48 mutli home terraform cloud (#49)
    * 48 wip
    
    * 48 fix issues with provider, setup two different towns

--- .terraform.lock.hcl
+++ .terraform.lock.hcl
@@ -5,7 +5,7 @@ provider "local.providers/local/terratowns" {
   version     = "1.0.0"
   constraints = "1.0.0"
   hashes = [
-    "h1:h1MWcYf3b9y/xFaFNW8g2MyKBSPMNZGUQEtI7BpD1ig=",
+    "h1:zWS9Erob9kbHgZ1gn1VLvuqXHNbgpPg4cwqg3/ZnuZY=",
   ]
 }
 
--- main.tf
+++ main.tf
@@ -6,13 +6,13 @@ terraform {
     }
   }
 
-#   cloud {
-#     organization = "TerraCamp"
+  cloud {
+    organization = "TerraCamp"
 
-#     workspaces {
-#       name = "terra-house-1"
-#     }
-#   }
+    workspaces {
+      name = "terra-house-1"
+    }
+  }
 }
 
 provider "terratowns" {
@@ -21,17 +21,17 @@ provider "terratowns" {
   token = var.terratowns_access_token
 }
 
-module "terrahouse_aws" {
-  source = "./modules/terrahouse-aws"
+module "home_games_hosting" {
+  source = "./modules/terrahome_aws"
   user_uuid = var.teacherseat_user_uuid
-  # bucket_name = var.bucket_name
-  index_html_filepath = var.index_html_filepath
-  error_html_filepath = var.error_html_filepath
-  content_version = var.content_version
-  assets_path = var.assets_path
+  public_path = var.games.public_path
+  # index_html_filepath = var.index_html_filepath
+  # error_html_filepath = var.error_html_filepath
+  # assets_path = var.assets_path
+  content_version = var.games.content_version
 }
 
-resource "terratowns_home" "home" {
+resource "terratowns_home" "home_games_hosting" {
   name = "Best noir comuter games. Mafia. L.A. Noir and other games."
   description = <<DESCRIPTION
 The noir genre arose in film during and after World War II. 
@@ -41,10 +41,34 @@ Its rise gave us some of the genre's earliest films like The
 Maltese Falcon and Laura, both of which were detective-based films, 
 which is why crime and detective work have often featured as part of the noir premise. 
 However, the genre has since spread to a variety of mediums, including video games. 
-Now there are numerous games that classify in the noir genre
+Now there are numerous games that classify in the noir genre.
 DESCRIPTION
   # domain_name =  "3eqdda2.cloudfront.net" 
-  domain_name = module.terrahouse_aws.cloudfront_url
+  domain_name = module.home_games_hosting.domain_name
   town = "missingo"
-  content_version = 1
+  content_version = var.games.content_version
+}
+
+module "home_movies_hosting" {
+  source = "./modules/terrahome_aws"
+  user_uuid = var.teacherseat_user_uuid
+  public_path = var.movies.public_path
+  # index_html_filepath = var.index_html_filepath
+  # error_html_filepath = var.error_html_filepath
+  # assets_path = var.assets_path
+  content_version = var.movies.content_version
+}
+
+resource "terratowns_home" "home_movies" {
+  name = "Best noir movies."
+  description = <<DESCRIPTION
+film noir, (French: “dark film”) style of filmmaking 
+characterized by such elements as cynical heroes, 
+stark lighting effects, frequent use of flashbacks, 
+intricate plots, and an underlying existentialist philosophy
+DESCRIPTION
+  # domain_name =  "3eqdda2.cloudfront.net" 
+  domain_name = module.home_movies_hosting.domain_name
+  town = "missingo"
+  content_version = var.movies.content_version
 }
